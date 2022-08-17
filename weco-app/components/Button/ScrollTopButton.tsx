2aa10966ff6ebfeec86ba3ccaa7c9e44635c9fae
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-08-17 15:53:24 +0900

    fix : scrollY=0 일때 버튼삭제
    scrollY 가 0 일때는 버튼 안보이게 하기

--- weco-app/components/Button/ScrollTopButton.tsx
+++ weco-app/components/Button/ScrollTopButton.tsx
@@ -1,6 +1,20 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export function ScrollTopButton() {
+  const [ScrollY, setScrollY] = useState<number>(0); // 스크롤값을 저장하기 위한 상태
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset); // window 스크롤 값을 ScrollY에 저장
+  };
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow);
+    };
+    watch(); // addEventListener 함수를 실행
+    return () => {
+      window.removeEventListener('scroll', handleFollow); // addEventListener 함수를 삭제
+    };
+  });
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -8,7 +22,9 @@ export function ScrollTopButton() {
     });
   };
   return (
-    <div className="fixed right-4 bottom-4 w-16 h-16 ">
+    <div
+      className={ScrollY === 0 ? 'hidden' : 'fixed right-4 bottom-4 w-16 h-16'}
+    >
       <Image
         className="hover:cursor-pointer hover:scale-110 ease-in-out duration-200"
         src="/img/top.jpg"
