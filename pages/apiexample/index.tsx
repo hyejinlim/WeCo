529a2d9b73f41cc508a0e60b2543ba2ffbc6fb8b
dongmin <rlaehdals753@naver.com>
2022-08-29 01:32:34 +0900

    feat: 테스트용 api 추가

--- pages/apiexample/index.tsx
+++ pages/apiexample/index.tsx
@@ -1,15 +1,7 @@
 import React, { useEffect } from 'react';
-import { getAuthToken } from 'services/AuthService';
-import {
-  getTest,
-  getTest2,
-  getTest3,
-  postTest,
-  putTest,
-  delTest,
-  useQueryGetTest,
-} from 'services/Test';
-import { useQuery } from 'react-query';
+import { useQueryGetTest } from 'services/Test';
+import { getComment } from 'services/CommentService';
+
 /**
  * api 호출 테스트 페이지 입니다.
  */
@@ -18,58 +10,12 @@ const SampleApiTest = () => {
   console.log('testData', testData);
 
   useEffect(() => {
-    getTestApi();
-    // getTest2Api();
-    // getTest3Api();
-    // postTestApi();
-    // putTestApi();
-    // deleteTestApi();
-    // updateAuthToken();
+    getComent();
   }, []);
 
-  const getTestApi = async () => {
-    const test = await getTest({ id: 1, name: 'test' });
-    console.log('getTestApi response test ===', test);
-  };
-
-  const getTest2Api = async () => {
-    const id = 1;
-    const name = 'test';
-    const test = await getTest2(id, name);
-    console.log('getTest3Api response test ===', test);
-  };
-
-  const getTest3Api = async () => {
-    const param = {
-      id: 1,
-      name: 'test',
-    };
-    const test = await getTest3(param);
-    console.log('getTest2Api response test ===', test);
-  };
-
-  const postTestApi = async () => {
-    const param = {
-      id: 1,
-      name: 'test',
-    };
-    const test = await postTest(param);
-    console.log('postTestApi response test ===', test);
-  };
-
-  const putTestApi = async () => {
-    const test = await putTest({ id: 1, name: 'test' });
-    console.log('putTestApi response test ===', test);
-  };
-
-  const deleteTestApi = async () => {
-    const test = await delTest({ id: 1, name: 'test' });
-    console.log('deleteTestApi response test ===', test);
-  };
-
-  const updateAuthToken = async () => {
-    const authToken = await getAuthToken();
-    console.log('updateAuthToken authToken ===', authToken);
+  const getComent = async () => {
+    const result = await getComment();
+    console.log('getComent ===', result);
   };
 
   return (
