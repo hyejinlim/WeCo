7629f7872bf1c1dea58e363854aba16a93c9b020
dongmin <rlaehdals753@naver.com>
2022-08-29 01:31:54 +0900

    feat: 쿠키 utill 함수 추가

--- utils/common.tsx
+++ utils/common.tsx
@@ -1,7 +1,28 @@
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
 export const addZero = (date: number) => {
   if (date < 10) {
     const zeroDate = ('00' + date).slice(-2);
     return zeroDate;
   }
   return date;
+};
+
+export const setCookie = async (name: string, value: string, option?: any) => {
+  return await cookies.set(name, value, { ...option });
+};
+
+export const getCookie = (name: string) => {
+  const cookie = cookies.get(name);
+  return cookie ? cookie : '';
+};
+
+export const isCookieExist = (name: string) => {
+  const isCookie = getCookie(name);
+  if (isCookie) {
+    return true;
+  }
+  return false;
 };
