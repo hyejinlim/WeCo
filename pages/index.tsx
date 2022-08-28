034f9ffa1b3be04df7fdc7481b36c1768081a6b4
dongmin <rlaehdals753@naver.com>
2022-08-29 01:17:18 +0900

    fix: 사용하지 않는것 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,7 @@ import { EvaluateModal } from 'components/modal/EvaluateModal';
 import { SkillSelect } from 'components/Skill';
 import { MainSlider } from 'components/Slider';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import RecruitMain from './recruit';
-import RecruitCard from './recruit/RecruitCard';
-import RecruitHeader from './recruit/RecruitHeader';
 
 const Home: NextPage = () => {
   return (
