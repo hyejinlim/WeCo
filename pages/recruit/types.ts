a6627f1cff532c698c30ba02d8f251000a5fb94d
hyejinlim <yoon1440_@naver.com>
2023-01-07 15:33:13 +0900

    🏷️ 카테고리, 리스트 타입 분리

---
+++ pages/recruit/types.ts
@@ -0,0 +1,18 @@
+export type Category = {
+  label: string;
+  value: string;
+  img: string;
+};
+
+export type Recruit = {
+  id: number;
+  type: string;
+  startDate: string;
+  title: string;
+  tag: string[];
+  language: string[];
+  author: string;
+  views: string;
+  comments: string;
+  isClosed: boolean;
+};
