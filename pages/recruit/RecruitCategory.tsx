76a7d278e4fe2511c978c28ccbaf02beb0c563a7
hyejinlim <yoon1440_@naver.com>
2023-01-07 15:53:03 +0900

    💄 선택 카테고리 색상 변경

--- pages/recruit/RecruitCategory.tsx
+++ pages/recruit/RecruitCategory.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
 import * as R from 'ramda';
 import { CATEGORY_LIST } from './constants';
 import { Category } from './types';
@@ -8,18 +8,22 @@ type Props = {
 };
 
 function RecruitCategory({ onCategoryChange }: Props) {
+  const [type, setType] = useState<string>('all');
   const handleCategoryChange = (category: Category) => {
     const { value } = category;
+    setType(value);
     onCategoryChange && onCategoryChange(value);
   };
   return (
     <section className="flex">
       {R.addIndex(R.map)((category: any, index: number) => {
-        const { label, img } = category;
+        const { label, value, img } = category;
         return (
           <div
             key={index}
-            className="flex items-center mr-6 font-bold text-2xl cursor-pointer text-neutral-400"
+            className={`flex items-center mr-6 font-bold text-2xl cursor-pointer ${
+              type === value ? 'text-black' : 'text-neutral-400'
+            }`}
             onClick={handleCategoryChange.bind(null, category)}
           >
             <img className="w-[28px] h-[28px]" src={img} alt={label} />
