33d1ee6cfed23e4a1b46d0aeb31bbb4d97f05a8e
hyejinlim <yoon1440_@naver.com>
2023-04-03 15:34:04 +0900

    🔧 firebase config

---
+++ pages/firebase-config.ts
@@ -0,0 +1,18 @@
+// https://firebase.google.com/docs/web/setup?hl=ko#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: 'weco-50888',
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const fireStore = getFirestore(firebase);
+export default fireStore;
