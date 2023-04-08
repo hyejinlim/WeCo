96312734b590a15884b686905ca48217f806af68
hyejinlim <yoon1440_@naver.com>
2023-04-08 14:04:02 +0900

    ♻️ SelectForm

--- components/select/SelectForm.tsx
+++ components/select/SelectForm.tsx
@@ -1,49 +1,37 @@
 import React from 'react';
 import Select, { GroupBase, OptionsOrGroups } from 'react-select';
 
-interface Props {
+type Props = {
   options: OptionsOrGroups<any, GroupBase<any>>;
-  isMulti: boolean;
+  isMulti?: boolean;
   placeHolder: string;
   onChange: (e: any) => void;
-  useId: () => string;
-}
-// TODO: key값 에러 처리
-const SelectForm = ({
+  [key: string]: any;
+};
+
+function SelectForm({
   options,
-  isMulti,
+  isMulti = false,
   placeHolder,
   onChange,
-  useId,
-}: Props) => {
+  ...args
+}: Props) {
   const customStyles = {
     control: (base: any) => ({
       ...base,
       minHeight: 56,
     }),
   };
-  return options[0].type !== 'contact_type' ? (
+  return (
     <Select
-      instanceId={useId()}
       options={options}
       isMulti={isMulti}
       placeholder={placeHolder}
       onChange={onChange}
       styles={customStyles}
-      isOptionDisabled={(options) => options.disabled}
-    />
-  ) : (
-    <Select
-      instanceId={useId()}
-      options={options}
-      isMulti={isMulti}
-      placeholder={placeHolder}
-      onChange={onChange}
-      styles={customStyles}
-      defaultValue={{ value: options[0].value, label: options[0].label }}
-      // isOptionDisabled={(options) => options.disabled}
+      {...args}
     />
   );
-};
+}
 
 export default SelectForm;
