ebff0bf90fc99964f4be6b0b93e9ef4fb4522eee
hyejinlim <yoon1440_@naver.com>
2023-04-08 14:07:47 +0900

    ✨ 새글쓰기 firebase 연동

--- components/PostWrite/index.tsx
+++ components/PostWrite/index.tsx
@@ -1,8 +1,6 @@
 import dynamic from 'next/dynamic';
-import { useState, useId, memo } from 'react';
-import { postInfoData } from '../../data/postInfo.js';
-import dayjs from 'dayjs';
-import { ToastContainer, toast } from 'react-toastify';
+import { memo, useEffect } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
 import 'react-toastify/dist/ReactToastify.css';
 import * as R from 'ramda';
 import SelectForm from '../select/SelectForm';
@@ -10,234 +8,266 @@ import DatePickerForm from '../picker/DatePickerForm';
 import PostHeader from 'components/Header/PostHeader';
 import PostButton from 'components/Button/PostButton';
 import TextField from 'components/TextField/TextField';
+import { useForm } from 'react-hook-form';
+import {
+  ContactType,
+  Period,
+  Positions,
+  ProgressType,
+  RecruitCnt,
+  RecruitType,
+  Skills,
+} from './spread';
+import { getPostFormSchema } from 'utils/validations/postValidation';
+import { doc, setDoc } from 'firebase/firestore';
+import { fireStore } from '../../firebase/firebaseClient';
+import { useAuth } from 'context/authProvider';
+import { useRouter } from 'next/router';
 
 const Editor = dynamic(() => import('../editor/Editor'), {
   ssr: false,
 });
+
 const InitialData = {
-  title: '',
-  content: '',
-  recruitType: '',
-  recruitCnt: '',
-  progressType: '',
-  duration: '',
-  contactType: '카카오톡 오픈채팅',
-  contact: '',
-  startDate: dayjs().format('YYYY-MM-DD'),
-  skills: [],
+  title: '', // 제목
+  content: '', // 내용
+  recruitType: '', // 모집 구분
+  recruitCnt: '', // 모집 인원
+  progressType: '', // 진행 방식
+  period: '', // 진행 기간
+  contactType: 'kakao', // 연락 방법
+  contactDetail: '', // 연락 방법 상세
+  endDate: dayjs().format('YYYY-MM-DD'), // 모집 마감일
+  skills: [], // 기술 스택
+  positions: [], // 모집 포지션
 };
 
-type RequestDataProps = {
-  [key: string]: any;
-} & typeof InitialData;
-
 function PostWrite() {
-  const [postData, setPostData] = useState(postInfoData);
-  const [requestData, setRequestData] = useState<RequestDataProps>(InitialData);
+  const user = useAuth();
+  const router = useRouter();
+  const {
+    register,
+    setValue,
+    getValues,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<typeof InitialData>({
+    resolver: getPostFormSchema(),
+  });
+
+  watch('contactType');
+  watch('endDate');
 
   /**
    * handler
    */
-  const handleSelected = (data: any) => {
-    const [value, type] = searchSkillValue(data);
-    setRequestData({ ...requestData, [type]: value });
+  const handleSelected = (type: any, data: any) => {
+    // TODO: includes
+    if (R.includes(type, ['skills', 'positions'])) {
+      const values = R.map(R.prop('value'))(data);
+      return setValue(type, values, { shouldValidate: true });
+    }
+    const { value } = data;
+    setValue(type, value, { shouldValidate: true });
   };
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePeriod = (type: any, value: Dayjs) => {
+    setValue(type, dayjs(value).format('YYYY-MM-DD'));
+  };
+  const handleChange = (e: any) => {
     const { name, value } = e.target;
-    setRequestData({ ...requestData, [name]: value });
+    setValue(name, value, { shouldValidate: true });
   };
   const handleContentChange = (value: string) => {
-    setRequestData({ ...requestData, content: value });
+    setValue('content', value);
   };
-  const handlePeriod = (value: any) => {
-    setRequestData({
-      ...requestData,
-      startDate: dayjs(value).format('YYYY-MM-DD'),
+
+  const handleClick = async (values: any) => {
+    const timeStamp = dayjs().valueOf().toString();
+    const docRef = doc(fireStore, user?.uid as string, timeStamp);
+    await setDoc(docRef, values).then(() => {
+      alert('등록되었습니다.');
+      router.push('/');
     });
   };
-  const handleClick = () => {
-    const errorKey = searchKey();
-    if (errorKey) {
-      showErrorToast(errorKey);
-    } else {
-      console.log('성공');
-      // api
-    }
-  };
 
-  const searchSkillValue = (data: any) => {
-    let newValue;
-
-    if (R.length(data) > 0) {
-      const skillValues = data.map(({ value }: any) => value);
-      newValue = skillValues;
-    } else {
-      newValue = [];
-    }
-
-    return [newValue, 'skills'];
-  };
-  const searchKey = (): string => {
-    let resultKey = '';
-    const keyArray: Array<string> = Object.keys(requestData);
-
-    keyArray.some((key) => {
-      if (key === 'startDate') {
-        const isNan = R.isNil(requestData[key]);
-        if (isNan) {
-          resultKey = 'errorStartDate';
-          return;
-        }
-      }
-      if (R.length(requestData[key]) < 1) {
-        resultKey = key;
-        return;
-      }
-    });
-    return resultKey;
-  };
-  const showErrorToast = (key: string) => {
-    let message = '';
-    switch (key) {
-      case 'title':
-        message = '제목을 입력해주세요!';
-        break;
-      case 'content':
-        message = '내용을 입력해주세요!';
-        break;
-      case 'recruitType':
-        message = '모집 구분을 선택해주세요!';
-        break;
-      case 'recruitCnt':
-        message = '모집 인원을 선택해주세요!';
-        break;
-      case 'progressType':
-        message = '진행 방식을 선택해주세요!';
-        break;
-      case 'duration':
-        message = '진행 기간을 선택해주세요!';
-        break;
-      case 'skills':
-        message = '기술 스택을 선택해주세요!';
-        break;
-      case 'startDate':
-        message = '시작일을 입력해주세요!';
-        break;
-      case 'errorStartDate':
-        message = '시작일을 알맞은 형태에 맞게 작성해주세요!';
-        break;
-      case 'contact':
-        message = '연락 주소를 입력해주세요!';
-        break;
-      default:
-    }
-    toast.error(message);
-  };
+  /**
+   * useEffect
+   */
+  useEffect(() => {
+    R.pipe(
+      R.toPairs,
+      R.forEach(([name, value]: any) => {
+        register(name);
+        setValue(name, value);
+      })
+    )(InitialData);
+  }, [InitialData, register, setValue]);
 
   return (
-    <>
-      <div className="custom_md:w-full flex flex-col w-[1024px] w-max-[1040px] mx-auto my-0 gap-12 py-14 px-4">
-        <section>
-          <PostHeader num={1} title="프로젝트 기본 정보를 입력해 주세요." />
-          {postData.map((group, index: number) => {
-            return (
-              <ul
-                key={index}
-                className={`flex gap-4 sm:flex-col ${
-                  index === 4 ? 'sm:-mt-6' : 'mt-10 sm:mt-5'
-                }`}
-              >
-                {group.map((item) => {
-                  const { id, uiType, title, options, isMulti, place_holder } =
-                    item;
-                  return uiType === 'default' ? (
-                    <li className="grow shrink basis-[0%]" key={id}>
-                      <label className="inline-block font-medium mb-1">
-                        {title}
-                      </label>
-                      <SelectForm
-                        useId={() => useId()}
-                        options={options}
-                        isMulti={isMulti}
-                        placeHolder={place_holder}
-                        onChange={handleSelected}
-                      />
-                    </li>
-                  ) : uiType === 'calendar' ? (
-                    <li className="grow shrink basis-[0%]" key={id}>
-                      <div className="flex flex-col">
-                        <div className="inline-block font-medium mb-1">
-                          {title}
-                        </div>
-                        <DatePickerForm
-                          onChange={handlePeriod}
-                          date={requestData.startDate}
-                        />
-                      </div>
-                    </li>
-                  ) : uiType === 'custom' ? (
-                    <li className="grow shrink basis-[0%]" key={id}>
-                      <div className="mt-3">
-                        <TextField
-                          name="contact"
-                          value={requestData.contact}
-                          placeholder="오픈 카톡방 링크"
-                          onChange={handleChange}
-                        />
-                      </div>
-                    </li>
-                  ) : (
-                    <li className="grow shrink basis-[0%]" key={id}>
-                      <div>
-                        <input
-                          className="w-full h-14 min-h-14 leading-10 pl-4 pr-12 py-3 border-2 border-solid border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-                          type="hidden"
-                        />
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            );
-          })}
-        </section>
-        <section>
-          <PostHeader num={2} title="프로젝트에 대해 소개해주세요." />
-          <section>
-            <label className="inline-block mb-1 font-bold" htmlFor="input">
-              제목
-            </label>
-            <TextField
-              name="title"
-              placeholder="글 제목을 입력해주세요!"
-              value={requestData.title}
-              onChange={handleChange}
+    <div className="custom_md:w-full flex flex-col w-[1024px] w-max-[1040px] mx-auto my-0 gap-12 py-14 px-4">
+      <section>
+        <PostHeader num={1} title="프로젝트 기본 정보를 입력해 주세요." />
+        <div className="gap-4 sm:flex-col grid-cols-2 grid">
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">모집 구분</label>
+            <SelectForm
+              name="recruitType"
+              options={RecruitType}
+              placeHolder="스터디/프로젝트"
+              onChange={handleSelected.bind(null, 'recruitType')}
             />
-            <div className="mt-4">
-              <Editor
-                value={requestData.content}
-                onChange={handleContentChange}
-              />
+            {errors?.recruitType && (
+              <div className="text-sm text-red-500">
+                {errors?.recruitType.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">모집 인원</label>
+            <SelectForm
+              name="recruitCnt"
+              options={RecruitCnt}
+              placeHolder="인원 미정~10명 이상"
+              onChange={handleSelected.bind(null, 'recruitCnt')}
+            />
+            {errors?.recruitCnt && (
+              <div className="text-sm text-red-500">
+                {errors?.recruitCnt.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">진행 방식</label>
+            <SelectForm
+              name="progressType"
+              options={ProgressType}
+              placeHolder="온라인/오프라인"
+              onChange={handleSelected.bind(null, 'progressType')}
+            />
+            {errors?.progressType && (
+              <div className="text-sm text-red-500">
+                {errors?.progressType.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">진행 기간</label>
+            <SelectForm
+              name="period"
+              options={Period}
+              placeHolder="기간 미정~6개월 이상"
+              onChange={handleSelected.bind(null, 'period')}
+            />
+            {errors?.period && (
+              <div className="text-sm text-red-500">
+                {errors?.period.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">기술 스택</label>
+            <SelectForm
+              name="skills"
+              isMulti
+              options={Skills}
+              placeHolder="프로젝트 사용 스택"
+              onChange={handleSelected.bind(null, 'skills')}
+            />
+            {errors?.skills && (
+              <div className="text-sm text-red-500">
+                {errors?.skills.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">모집 마감일</label>
+            <DatePickerForm
+              date={getValues('endDate')}
+              onChange={handlePeriod.bind(null, 'endDate')}
+            />
+            {errors?.endDate && (
+              <div className="text-sm text-red-500">
+                {errors?.endDate.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">모집 포지션</label>
+            <SelectForm
+              name="positions"
+              isMulti
+              options={Positions}
+              placeHolder="프론트엔드, 백엔드..."
+              onChange={handleSelected.bind(null, 'positions')}
+            />
+            {errors?.positions && (
+              <div className="text-sm text-red-500">
+                {errors?.positions.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            <label className="font-medium">연락 방법</label>
+            <SelectForm
+              name="contactType"
+              defaultValue={ContactType[0]}
+              options={ContactType}
+              placeHolder="연락 방법"
+              onChange={handleSelected.bind(null, 'contactType')}
+            />
+            <TextField
+              name="contactDetail"
+              value={getValues('contactDetail')}
+              placeholder={
+                getValues('contactType') === 'kakao'
+                  ? '오픈 카톡방 링크'
+                  : getValues('contactType') === 'google'
+                  ? '이메일'
+                  : '구글 폼 주소'
+              }
+              onChange={handleChange}
+              errorText={errors?.contactDetail?.message}
+            />
+          </div>
+        </div>
+      </section>
+      <section>
+        <PostHeader num={2} title="프로젝트에 대해 소개해주세요." />
+        <section className="flex flex-col gap-2">
+          <label className="font-medium" htmlFor="input">
+            제목
+          </label>
+          <TextField
+            name="title"
+            placeholder="글 제목을 입력해주세요!"
+            value={getValues('title')}
+            onChange={handleChange}
+            errorText={errors?.title?.message}
+          />
+          <Editor value={getValues('content')} onChange={handleContentChange} />
+          {errors?.content && (
+            <div className="mt-10 text-sm text-red-500">
+              {errors?.content.message}
             </div>
-          </section>
+          )}
         </section>
-        <section className="flex justify-end items-center mt-4">
-          <PostButton
-            type="submit"
-            buttonName="취소"
-            backgroundColor="bg-slate-200"
-          />
-          <PostButton
-            type="button"
-            buttonName="글 등록"
-            backgroundColor="bg-black"
-            textColor="text-white"
-            onClick={handleClick}
-          />
-        </section>
-      </div>
-      <ToastContainer theme="colored" position="top-right" autoClose={2000} />
-    </>
+      </section>
+      <section className="flex justify-end items-center mt-4">
+        <PostButton
+          type="submit"
+          buttonName="취소"
+          backgroundColor="bg-slate-200"
+        />
+        <PostButton
+          type="button"
+          buttonName="글 등록"
+          backgroundColor="bg-black"
+          textColor="text-white"
+          onClick={handleSubmit(handleClick)}
+        />
+      </section>
+    </div>
   );
 }
 
