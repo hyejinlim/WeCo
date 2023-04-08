f8734d43de2e4a34547dffa3151c6eb3a0e7b02a
hyejinlim <yoon1440_@naver.com>
2023-04-08 14:05:25 +0900

    ♻️ TextField errorText 추가

--- components/TextField/TextField.tsx
+++ components/TextField/TextField.tsx
@@ -1,12 +1,14 @@
 import { memo, useEffect, useState } from 'react';
+import { FieldError, FieldErrorsImpl, Merge } from 'react-hook-form';
 
 type Props = {
   name?: string;
   value?: string;
   placeholder?: string;
+  errorText?: string | FieldError | Merge<FieldError, FieldErrorsImpl<any>>;
   onChange?: (e: any) => void;
 };
-function TextField({ name, value, placeholder, onChange }: Props) {
+function TextField({ name, value, placeholder, errorText, onChange }: Props) {
   const [text, setText] = useState<string>('');
 
   const handleChange = (e: any) => {
@@ -23,14 +25,21 @@ function TextField({ name, value, placeholder, onChange }: Props) {
   useEffect(update, [value]);
 
   return (
-    <input
-      type="text"
-      name={name}
-      className="w-full h-14 min-h-14 leading-10 pl-4 pr-12 py-3 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-      placeholder={placeholder}
-      value={text}
-      onChange={handleChange}
-    />
+    <div>
+      <input
+        type="text"
+        name={name}
+        className={`w-full h-14 min-h-14 leading-10 pl-4 pr-12 py-3 border rounded focus:outline-none focus:border-blue-500 ${
+          !!errorText ? 'border-red-500' : 'border-gray-300'
+        }`}
+        placeholder={placeholder}
+        value={text}
+        onChange={handleChange}
+      />
+      {!!errorText && (
+        <p className="text-red-500 text-sm mt-2">{errorText.toString()}</p>
+      )}
+    </div>
   );
 }
 export default memo(TextField);
