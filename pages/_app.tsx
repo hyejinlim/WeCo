c0ed8952bba79d219216b7f56dd92b7b7299b9e9
hyejinlim <yoon1440_@naver.com>
2023-04-09 17:44:50 +0900

    🔥 미사용 코드 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,12 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
-import { setAuthToken } from 'services/BaseService';
-import { useEffect } from 'react';
-import { tempAuthLogin } from 'services/LoginService';
-import { setCookie, isCookieExist } from 'utils/common';
 import { AuthProvider } from 'context/authProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
-
-  useEffect(() => {
-    const isCookie = isCookieExist('accessToken');
-    if (!isCookie) {
-      tempLogin();
-    } else {
-    }
-  }, []);
-
-  const tempLogin = async () => {
-    const response = await tempAuthLogin();
-    if (response.data.accessToken) {
-      const accessToken = response.data.accessToken;
-
-      const expires = new Date();
-      expires.setDate(Date.now() + 1000 * 60 * 60 * 24); //24시간
-
-      setAuthToken(accessToken); //header
-      setCookie('accessToken', accessToken, {
-        path: '/',
-        expires,
-      }); //cookie
-    }
-  };
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </QueryClientProvider>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   );
 }
 
